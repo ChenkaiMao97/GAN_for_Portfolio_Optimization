41fb4fcd5d124d39af7b429bbb5b6cf399c587fd
mildred-tfm <tfmin127@stanford.edu>
2021-12-02 22:58:41 -0800

    add Markowitz.py

---
+++ Markowitz.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cvxopt as opt
+from cvxopt import blas, solvers
+import matplotlib.pyplot as plt
+# n_assets = 4
+k = 15
+n_obs = 40
+returns = np.random.randn(k, n_obs)
+
+def get_mu_sigma(returns, rmax=1.15, Z=25):
+    n = len(returns)
+    returns = np.asmatrix(returns)
+
+    N = 100
+    #desired returns
+    mus = [10 ** (2.0 * t / N - 1.0) for t in range(25)]
+    # mus = np.array([1] + [1 + (t - 1)*(rmax - 1)/(Z-1) for t in range(2, 25)]) -1
+    # Convert to cvxopt matrices
+    S = opt.matrix(np.cov(returns))
+    pbar = opt.matrix(np.mean(returns, axis=1))
+
+    # Create constraint matrices
+    G = -opt.matrix(np.eye(n))  # negative n x n identity matrix
+    h = opt.matrix(0.0, (n, 1))
+    A = opt.matrix(1.0, (1, n))
+    b = opt.matrix(1.0)
+
+    # Calculate efficient frontier weights using quadratic programming
+    portfolios = [solvers.qp(mu * S, -pbar, G, h, A, b)['x']
+                  for mu in mus]
+    ## CALCULATE RISKS AND RETURNS FOR FRONTIER
+    returns = [blas.dot(pbar, x) for x in portfolios]
+    risks = [np.sqrt(blas.dot(x, S * x)) for x in portfolios]
+    ## CALCULATE THE 2ND DEGREE POLYNOMIAL OF THE FRONTIER CURVE
+    m1 = np.polyfit(returns, risks, 2)
+    x1 = np.sqrt(m1[2] / m1[0])
+    # CALCULATE THE OPTIMAL PORTFOLIO
+    wt = solvers.qp(opt.matrix(x1 * S), -pbar, G, h, A, b)['x']
+    return np.asarray(wt), returns, risks
+
+
+weights, returns, risks = get_mu_sigma(returns)
+
+fig = plt.figure()
+plt.plot(risks, returns)
+plt.show()
+print(weights)
