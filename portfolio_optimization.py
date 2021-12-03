79d0ca5c2ad1e3f8399c60327751b91de4b27ea8
ChenkaiMao97 <chenkai@mit.edu>
2021-12-02 20:45:19 -0800

    sync

--- portfolio_optimization.py
+++ portfolio_optimization.py
@@ -65,4 +65,7 @@ for i, sample_batched in enumerate(trainer.train_loader):
     if i==0:
       break
 
+np.store("porfolio_data/fake_batches.npy", fake_batches.numpy())
+np.store("porfolio_data/real_batches.npy", real_batch.numpy())
+
 
